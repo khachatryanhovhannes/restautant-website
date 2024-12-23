d1f8c13ed0f1472854c01a78396d8eb012528d5e
hovhanneskhachatryan <166857475+hovhanneskhachatryan@users.noreply.github.com>
2024-12-23 13:13:26 +0400

    bugfix: Fix services section responsivity

--- components/home_services/index.tsx
+++ components/home_services/index.tsx
@@ -5,36 +5,31 @@ import { MdOutlineDeliveryDining } from "react-icons/md";
 export default function HomeServices() {
   return (
     <section className="bg-[#FFF7EA]">
-      <div className="flex flex-col lg:flex-row justify-center gap-14 py-20 items-center">
-        {/* Text Section */}
-        <div className="max-w-[400px] text-center sm:text-left">
+      <div className="flex justify-center gap-14 py-20 items-center" >
+        <div className="max-w-[400px]">
           <p>
             <span className="border-t border-b border-black text-[15px]">
               WHAT WE OFFER
             </span>
           </p>
-          <h3 className="text-3xl sm:text-4xl font-semibold my-8">
-            Our Great Services
-          </h3>
+          <h3 className="text-4xl font-semibold my-8">Our Great Services</h3>
           <p>
             Lorem Ipsum is that it has a more-or-less normal distribution
             content making it look like readable English.
           </p>
         </div>
-
-        {/* Icon Section */}
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 text-white">
-          <div className="text-center bg-gray-800 p-5 flex flex-col items-center rounded-lg shadow-md">
+        <div className="flex text-white gap-10">
+          <div className="text-center bg-gray-800 p-5 flex flex-col items-center">
             <MdOutlineMenuBook fontSize={60} color="#f5bb64" />
-            <p className="mt-4">Special Menus</p>
+            <p>Special Menus</p>
           </div>
-          <div className="text-center bg-gray-800 p-5 flex flex-col items-center rounded-lg shadow-md">
+          <div className="text-center bg-gray-800 p-5 flex flex-col items-center">
             <PiCalendar fontSize={60} color="#f5bb64" />
-            <p className="mt-4">Opened 24/7</p>
+            <p>Opened 24/7</p>
           </div>
-          <div className="text-center bg-gray-800 p-5 flex flex-col items-center rounded-lg shadow-md">
-            <MdOutlineDeliveryDining fontSize={60} color="#f5bb64" />
-            <p className="mt-4">Home Delivery</p>
+          <div className="text-center bg-gray-800 p-5 flex flex-col items-center">
+            <MdOutlineDeliveryDining  fontSize={60} color="#f5bb64" />
+            <p>Home Delivery</p>
           </div>
         </div>
       </div>
